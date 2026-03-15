8ca3fc4d8c0f8ba86860202c441d8e9f10ccd902
SilverMight <joeyzee1234@gmail.com>
2026-03-15 13:46:55 -0400

    fix: add trusted users for remote build

--- common/default.nix
+++ common/default.nix
@@ -3,6 +3,8 @@
 {
   # flakes on for all
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.settings.trusted-users = [ "root" "silvermight" ];
   
   # Shared User configuration
   users.users.silvermight = {
@@ -18,4 +20,5 @@
   time.timeZone = "America/New_York";
 
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
 }
