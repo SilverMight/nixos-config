02a6bec5550200a62d0d388efb50acf64548e2e1
SilverMight <joeyzee1234@gmail.com>
2026-08-22 20:40:22 -0400

    fix: use nixpkgs binary cache w/ overlay for deploy-rs, remove headplane flake

--- flake.nix
+++ flake.nix
@@ -6,22 +6,33 @@
     
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    
-    headplane.url = "github:tale/headplane";
-    headplane.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    system = "aarch64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    deployPkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        inputs.deploy-rs.overlays.default
+        (self: super: {
+          deploy-rs = {
+            inherit (pkgs) deploy-rs;
+            lib = super.deploy-rs.lib;
+          };
+        })
+      ];
+    };
+  in {
     nixosConfigurations = {
       rpi4 = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
+        inherit system;
         specialArgs = { inherit inputs; };
         modules = [
           inputs.sops-nix.nixosModules.sops
@@ -30,7 +41,7 @@
       };
       
       vps = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
+        inherit system;
         specialArgs = { inherit inputs; };
         modules = [
           inputs.disko.nixosModules.disko
@@ -41,14 +52,12 @@
     };
 
     deploy.nodes = {
-
-
       rpi4 = {
         hostname = "rpi.local";
         profiles.system = {
           remoteBuild = true;
           sshUser = "silvermight";
-          path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.rpi4;
+          path = deployPkgs.deploy-rs.lib.activate.nixos self.nixosConfigurations.rpi4;
           user = "root";
         };
       };
@@ -58,7 +67,7 @@
         profiles.system = {
           remoteBuild = true;
           sshUser = "silvermight";
-          path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.vps;
+          path = deployPkgs.deploy-rs.lib.activate.nixos self.nixosConfigurations.vps;
           user = "root";
         };
       };
